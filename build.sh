af8da1e064be1e488f8ea86e03c2faf03b334eb5
Matt Haggard <haggardii@gmail.com>
2017-01-19 11:43:09 -0700

    Build for linux

--- build.sh
+++ build.sh
@@ -2,5 +2,18 @@
 # Copyright (c) The LHTML team
 # See LICENSE for details.
 
-# mac
-electron-packager . LHTML --platform=darwin --arch=all --version=1.2.5 --out=dist --overwrite --icon resources/mac/lhtml.icns --extra-resource resources/mac/lhtml.icns --extend-info resources/mac/fileextensions.plist
+target=$1
+
+if [ -z "$target" ]; then
+    target="mac"
+fi
+
+if [ "$target" = "mac" ]; then
+    # mac
+    electron-packager . LHTML --platform=darwin --arch=all --version=1.2.5 --out=dist --overwrite --icon resources/mac/lhtml.icns --extra-resource resources/mac/lhtml.icns --extend-info resources/mac/fileextensions.plist
+fi
+
+if [ "$target" = "linux" ]; then
+    # linux
+    electron-packager . LHTML --platform=linux --arch=all --version=1.2.5 --out=dist --overwrite
+fi
