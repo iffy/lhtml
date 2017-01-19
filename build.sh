fd76d04d78a2d40488e50b35e45bc81573b41cab
Matt Haggard <haggardii@gmail.com>
2017-01-19 16:34:04 -0700

    Build for windows, too (untested)

--- build.sh
+++ build.sh
@@ -5,15 +5,20 @@
 target=$1
 
 if [ -z "$target" ]; then
-    target="mac"
+    target="all"
 fi
 
-if [ "$target" = "mac" ]; then
-    # mac
+# mac
+if [ "$target" = "mac" ] || [ "$target" = "all" ]; then
     electron-packager . LHTML --platform=darwin --arch=all --version=1.2.5 --out=dist --overwrite --icon resources/mac/lhtml.icns --extra-resource resources/mac/lhtml.icns --extend-info resources/mac/fileextensions.plist
 fi
 
-if [ "$target" = "linux" ]; then
-    # linux
+# linux
+if [ "$target" = "linux" ] || [ "$target" = "all" ]; then
     electron-packager . LHTML --platform=linux --arch=all --version=1.2.5 --out=dist --overwrite
+fi
+
+# windows
+if [ "$target" = "win" ] || [ "$target" = "all" ]; then
+    electron-packager . LHTML --platform=win --arch=all --version=1.2.5 --out=dist --overwrite
 fi
